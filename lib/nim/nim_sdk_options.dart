7f95919c5f31706565be030ad3690de2ccf3f1cf
skyHuang <858215130@qq.com>
2021-11-09 23:01:52 +0800

    添加挤下线功能

--- lib/nim/nim_sdk_options.dart
+++ lib/nim/nim_sdk_options.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:nim_core/nim_core.dart';
 import 'package:untitled/basic/include.dart';
+import 'package:untitled/nim/nim_network_manager.dart';
 import 'package:untitled/persistent/get_storage_utils.dart';
 
 Future<void> nimSdkInit() async {
@@ -21,17 +22,19 @@ Future<void> nimSdkInit() async {
   final options = Platform.isAndroid
       ? NIMAndroidSDKOptions(
           appKey: 'a43f20db62b5b8d8debdb207e01cdb47',
-
+          shouldSyncUnreadCount: true,
           /// 其他 通用/Android 配置
           autoLoginInfo: auto)
       : NIMIOSSDKOptions(
           appKey: 'a43f20db62b5b8d8debdb207e01cdb47', autoLoginInfo: auto,
-          apnsCername:'freedom push',  //推送证书名
+          apnsCername: 'freedom push', //推送证书名
+          shouldSyncUnreadCount: true,
           /// 其他通用配置/iOS 配置
         );
   NimCore.instance.initialize(options).then((result) {
     if (result.isSuccess) {
       logger.i('云信 initialize isSuccess info=$auto');
+      NimNetworkManager.instance.onlineClientsListener();
     } else {
       logger.e('云信initialize 失败info=$auto      '
           'errorDetails${result.errorDetails}'
--- lib/page/global_controller.dart
+++ lib/page/global_controller.dart
@@ -5,13 +5,13 @@ import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:nim_core/nim_core.dart';
 import 'package:untitled/network/logger.dart';
+import 'package:untitled/nim/nim_network_manager.dart';
 import 'package:untitled/persistent/get_storage_utils.dart';
 
 class GlobalController extends GetxController {
-  RxBool isSvip = GetStorageUtils
-      .getSvip()
-      .obs;
+  RxBool isSvip = GetStorageUtils.getSvip().obs;
 
   @override
   void onInit() {
